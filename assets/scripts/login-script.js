354ab8294ba3ba168d315863932102b38cd608b4
Kevin Christian -71210797 <93351620+kevinchritian@users.noreply.github.com>
2024-02-18 16:04:30 +0700

    Update login-script.js
    mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -6,20 +6,37 @@
  */
 
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+*membuat variabel inputEmailElement unutk tampilan input email
+*@constant (HTMLElement)
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+// membuat variabel inputPasswordElement unttuk tampilan password
+// @constan (HTMLElement)
 const inputPasswordElement = document.querySelector('#inputPassword');
 
 const expectedEmail = 'admin@dicoding.com';
+
+
+// membuat varabel expectedPassword unutk menyimpan informasi password
 const expectedPassword = 'superpassword';
 
+// menambahkan aksi klik pada Button
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  // membuat password unutk menyimapan niali password yang digunakan
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+  // cek apakah sama kondisi
   if (email == expectedEmail && password == expectedPassword) {
+    // maka ke halaman home
     goToHome();
   } else {
+    // jika tidak muncul pop up
     showPopUp();
   }
 });
